97da72325a08025bf80bbb51d0237b3e8038a60f
yasinabdmahmood <abd181103@gmail.com>
2023-01-10 23:12:56 +0300

    Create Messsage table/model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+messages = Message.create([
+    { content: "Hey there, just wanted to check in and see how you're doing!"},
+    { content: "Thanks for your help earlier today, it was much appreciated!"},
+    { content: "I'm sorry for the inconvenience, but could you please clarify the instructions?"},
+    { content: "I just wanted to remind you about the meeting scheduled for tomorrow at 10 AM."},
+    { content: "Congratulations on the promotion! Well deserved."},
+])
