266e4b703c608467714c3566bd84823148f9e46e
yasinabdmahmood <abd181103@gmail.com>
2023-01-11 00:48:38 +0300

    Prepare project for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'message#random_message'
   get 'message/random_message'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
