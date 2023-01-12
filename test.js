53edf3ad86cbc53226a2df566bb6166dd4ace4ae
yasinabdmahmood <abd181103@gmail.com>
2023-01-13 02:33:30 +0300

    Add some comments

--- test.js
+++ test.js
@@ -1,47 +1,4 @@
-// const http = require('http');
-// const data = JSON.stringify({name: 'John Smith', username: 'testusername', email: "john@gmail", password: "123456"});
-// const options = {
-//   hostname: 'api.example.com',
-//   port: 80,
-//   path: 'http://127.0.0.1:3000/users',
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//     'Content-Length': data.length
-//   }
-// };
-
-// const req = http.request(options, (res) => {
-//   console.log(`statusCode: ${res.statusCode}`);
-//   res.on('data', (d) => {
-//     process.stdout.write(d);
-//   });
-// });
-
-// req.on('error', (error) => {
-//   console.error(error);
-// });
-// req.write(data);
-// req.end();
-///////////////////
-// const request = new XMLHttpRequest();
-// const url = 'http://127.0.0.1:3000/users';
-// const data = {name: 'John Smith', username: 'testusername', email: "john@gmail", password: "123456"};
-
-// request.open('POST', url, true);
-// request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-
-// request.onreadystatechange = function () {
-//   if (request.readyState === 4 && request.status === 200) {
-//     console.log(request.responseText);
-//   }
-// };
-
-// request.send(JSON.stringify(data));
-
-
-////////////////////////////////
-
+// the code below makes sign in request by registering new user
 
 // const axios = require('axios');
 // axios.post('http://127.0.0.1:3000/users', {name: 'John Smith', username: 'testusername', email: "john@gmail", password: "123456"})
@@ -55,6 +12,8 @@
 
 ////////////////////////
 
+// the code below makes log in request
+
 // const axios = require('axios');
 // axios.post('http://127.0.0.1:3000/auth/login', {email: "john@gmail", password: "123456"})
 //   .then(response => {
@@ -64,10 +23,14 @@
 //     console.log(error);
 //   });
 
-  ///////////////////////////
-  // token: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NzQxNjc4NjJ9.H2Yp5Cjqzuhl4Nnw-IxS3o4yr0zReYtfvD_VDCovpxc'
-  ///////////////////////////////////////////
 
+///////////////////////////
+  
+
+
+// the below code makes api get request to fetch some resourses
+// it is worth mentioning that code in line 75 is making sure to send the token along with 
+// the request
 const axios = require('axios');
 axios.defaults.headers.common['Authorization'] = `Bearer ${"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NzQxNjg5MDd9.qHIgUDOz4SWqzVMhNFVvsH71RtaDsXYRl8GiQC-1_7k"}`;
 axios.get('http://127.0.0.1:3000/message/random_message')
